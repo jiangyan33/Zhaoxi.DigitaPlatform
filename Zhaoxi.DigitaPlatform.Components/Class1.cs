8bb8248512c03a8b82f2649ba25f8bb6aa348a58
jiangshan shi <1720319534@qq.com>
2023-07-03 22:35:27 +0800

    增加管道信息

--- Zhaoxi.DigitaPlatform.Components/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Zhaoxi.DigitaPlatform.Components
-{
-    public class Class1
-    {
-    }
-}
---
+++ Zhaoxi.DigitaPlatform.Components/Meter.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zhaoxi.DigitaPlatform.Components
+{
+    /// <summary>
+    /// Meter.xaml 的交互逻辑
+    /// </summary>
+    public partial class Meter : UserControl
+    {
+        public Meter()
+        {
+            InitializeComponent();
+        }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(nameof(Header), typeof(string), typeof(Meter), new PropertyMetadata(""));
+
+
+
+        public string Unit
+        {
+            get { return (string)GetValue(UnitProperty); }
+            set { SetValue(UnitProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Unit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UnitProperty =
+            DependencyProperty.Register(nameof(Unit), typeof(string), typeof(Meter), new PropertyMetadata(""));
+
+
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(nameof(Value), typeof(double), typeof(Meter), new PropertyMetadata(0.0));
+
+
+
+
+
+
+
+    }
+}
